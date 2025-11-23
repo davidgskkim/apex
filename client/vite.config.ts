9b2c93e01ebbc0b37f509dcd560f890ef528a863
DavidGSKim <davidgs.lee7@gmail.com>
2025-11-22 21:57:17 -0500

    Optimize performance

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    modulePreload: false, 
+    
     rollupOptions: {
       output: {
         manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom') || id.includes('node_modules/react-router-dom')) {
+            return 'vendor-react';
+          }
           if (id.includes('chart.js') || id.includes('react-chartjs-2')) {
-            return 'charts';
+            return 'vendor-charts';
           }
-          if (id.includes('node_modules/react/') || id.includes('node_modules/react-dom/')) {
-            return 'react-vendor';
-          }
-          if (id.includes('react-router')) {
-            return 'router-vendor';
+          if (id.includes('framer-motion') || id.includes('lucide-react')) {
+            return 'vendor-ui';
           }
         },
       },
